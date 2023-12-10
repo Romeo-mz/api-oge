4347672225bfa4ea6aafd40dc41c03604fa66b4c
Romeo-mz <romeo.martinez@orange.fr>
2023-12-10 22:38:55 +0100

    Refactor code to display absence data in a graph

--- abcenceViewer.py
+++ abcenceViewer.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
+from api import API
+from dataProcessing import format_absence_data
+import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='front/html', static_folder='front/js')
 
 @app.route('/')
-
 def home():
+    api = API()
+    api.login()
+    absences = api.getAllAbsences()
+    formatted_absences = format_absence_data(absences)
+
+    labels = [absence['course'] for absence in formatted_absences]
+    values = [absence['status'] for absence in formatted_absences]
+
     data = {
-        "name": "GARNIER LIONEL",
-        "hours": [15.30, 17.30],
-        "subject": "Maths",
-        "date": "19/01/2023"
+        'labels': json.dumps(labels),  # Convert Python list to JSON string
+        'values': json.dumps(values)   # Convert Python list to JSON string
     }
+    return render_template('graph.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
