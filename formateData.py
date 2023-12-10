e551497e8cc03eba49cdd135b039a3aa53e3e904
Romeo-mz <romeo.martinez@orange.fr>
2023-12-10 21:55:57 +0100

    Add function to process absence data

--- formateData.py
+++ formateData.py
@@ -2,8 +2,29 @@ from api import API
 
 def format_absence_data(absences):
     formatted_absences = []
-    # TO do
 
+    def process_absence(absence):
+        for entry in absence:
+            if entry:
+                if isinstance(entry[0], list):
+                    process_absence(entry)  # Recursively handle deeper levels
+                else:
+                    course_info = entry[0].split('\n')  # Split course info
+                    if len(course_info) == 2:
+                        course = course_info[0].strip()
+                        professor = entry[1].strip()
+                        date_time = entry[2].strip()
+                        status = entry[3].strip()
+                        formatted_absences.append({
+                            'course': course,
+                            'professor': professor,
+                            'date_time': date_time,
+                            'status': status
+                        })
+
+    process_absence(absences)
+    return formatted_absences
+     
 def main():
     api = API()
     api.login()
