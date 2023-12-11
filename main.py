b3744b3b6e9f23e353504aa99bc7f5b4ece530f2
Romeo-mz <romeo.martinez@orange.fr>
2023-12-11 17:39:38 +0100

    Remove unnecessary print statements and update semester iteration

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ def main():
     api = API()
     api.login()
     absences = api.getAllAbsences()
-    
-    # print(api.getAbsencesBySemester(1))
-    # print(api.getAbsencesBySemester(2))
+    print(absences)
     # print(api.getAbsencesBySemester(3))
+    
+    # print(api.getAbsencesBySemester(2))
+
+    # print(api.getAbsencesBySemester(1))
     
 if __name__ == "__main__":
     main()
